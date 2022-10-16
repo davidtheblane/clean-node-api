a684a374ad0b6c51627c78527a88106b0a2aaef3
Davi Bernardo <davidtheblane@gmail.com>
2022-10-16 08:18:08 -0300

    ensure LoginRouter returns 200 a valid token when valid credentials are provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -83,7 +83,7 @@ describe('Login Router', () => {
   })
 
   test('should return 200 when valid credentials are provided', () => {
-    const { sut } = makeSut()
+    const { sut, authUseCaseSpy } = makeSut()
     const httpRequest = {
       body: {
         email: 'valid_email@email.com',
@@ -92,6 +92,7 @@ describe('Login Router', () => {
     }
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body.accessToken).toEqual(authUseCaseSpy.accessToken)
   })
 
   test('should return 500 if no AuthUseCase is provided', () => {
